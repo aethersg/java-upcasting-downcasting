f4afb41e50cc0c1ba92874e5529f4335888931b7
jude <jude@affle.com>
2015-01-31 23:14:24 +0800

    initial commit

---
+++ JavaUpcastingDowncasting/src/Application.java
@@ -0,0 +1,53 @@
+
+class Machine
+    {
+    public void start()
+        {
+        System.out.println("Machine Started");
+        }
+    }
+
+class Camera
+        extends Machine
+    {
+    @Override
+    public void start()
+        {
+        System.out.println("Camera Started");
+        }
+
+    public void snap()
+        {
+        System.out.println("Photo Taken");
+        }
+    }
+
+public class Application
+    {
+    public static void main(String[] args)
+        {
+        Machine machine1 = new Machine();
+        Camera camera1 = new Camera();
+
+        machine1.start();
+        camera1.start();
+        camera1.snap();
+
+        // Upcasting
+        Machine machine2 = camera1;
+        machine2.start();
+        // error: machine2.snap();
+
+        //Downcasting is usually unsafe
+        Machine machine3 = new Camera();
+        Camera camera2 = (Camera) machine3;
+        camera2.start();
+        camera2.snap();
+
+        //does not work -- runtime error
+        //Machine machine4 = new Machine();
+        //Camera camera3 = (Camera) machine4;
+        //camera3.start();
+        //camera3.snap();
+        }
+    }
